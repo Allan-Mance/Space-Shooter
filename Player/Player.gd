c144c32f35ebb6b3a9a942bb055c777d27b08849
Allan-Mance <mance.allan@gmail.com>
2023-09-16 10:18:10 -0400

    Update Player.gd

--- Player/Player.gd
+++ Player/Player.gd
@@ -3,14 +3,23 @@ extends CharacterBody2D
 var speed = 5
 var max_speed = 400
 var rotate_speed = 0.1
-	
-func _physics_process(delta):
+var nose = Vector2(0,-80)
+
+func get_input():
+	var to_return = Vector2.ZERO
+	$Exhaust.hide()
+	if Input.is_action_pressed("Forward"):
+		to_return += Vector2(0,-1)
+		$Exhaust.show()
 	if Input.is_action_pressed("Left"):
-		rotation = rotation - rotate_speed
+		rotation -= rotate_speed
 	if Input.is_action_pressed("Right"):
 		rotation += rotate_speed
-	if Input.is_action_pressed("Forward"):
-		velocity = velocity + Vector2(0, -speed).rotated(rotation)
+		return to_return.rotated(rotation)
+	
+func _physics_process(delta):
+	velocity += get_input() * speed
+	velocity = velocity.normalized() * clamp(velocity.length(), 0, max_speed) 
 	
 	position.x = wrapf(position.x, 0, 1152)
 	position.y = wrapf(position.y, 0, 648)
