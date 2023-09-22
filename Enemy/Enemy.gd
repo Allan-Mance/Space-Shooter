725c7caa13f6c5af1140ece8934f2acec5d0b23c
Allan-Mance <mance.allan@gmail.com>
2023-09-22 11:33:04 -0400

    Fixed Enemy Shooting

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -25,7 +25,7 @@ func _on_timer_timeout():
 	var Effects = get_node_or_null("/root/Game/Effects")
 	if Player != null and Effects != null:
 		var bullet = Bullet.instantiate()
-		var d = global_position.angle_to_point(Player.global.position) + PI/2
+		var d = global_position.angle_to_point(Player.global_position) + PI/2
 		bullet.rotation = d
 		bullet.global_position = global_position + Vector2(0,-40).rotated(d)
 		Effects.add_child(bullet)
